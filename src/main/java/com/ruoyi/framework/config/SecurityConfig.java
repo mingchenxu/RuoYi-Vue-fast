911922a3a7cec6e4ead78acc4bee4d42f722b36c
mingchenxu <51768588@qq.com>
2022-01-07 10:19:25 +0800

    生产环境关闭swagger； 更换actuator端点；

--- src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -112,7 +112,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/webjars/**").anonymous()
                 .antMatchers("/*/api-docs").anonymous()
                 .antMatchers("/druid/**").anonymous()
-                .antMatchers("/actuator/**").anonymous()
+                .antMatchers("/probe/**").anonymous()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
