306c65d9f25e8dc492d09619f82e3bd11297c7ae
chenlin <v_chenlin3.js@chinatelecom.cn>
2024-04-03 15:25:37 +0800

    根据参数键名查询参数值加权限，普通用户无权限查询

--- src/main/java/com/ruoyi/project/system/controller/SysConfigController.java
+++ src/main/java/com/ruoyi/project/system/controller/SysConfigController.java
@@ -69,6 +69,7 @@ public class SysConfigController extends BaseController
     /**
      * 根据参数键名查询参数值
      */
+    @PreAuthorize("@ss.hasPermi('system:config:list')")
     @GetMapping(value = "/configKey/{configKey}")
     public AjaxResult getConfigKey(@PathVariable String configKey)
     {
